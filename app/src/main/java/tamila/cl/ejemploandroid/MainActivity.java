11ebe1304b2b8b02a48901d5330375e8ea18143f
César Cancino <yo@cesarcancino.com>
2018-05-13 16:10:14 -0400

    primer commit :facepunch:

---
+++ app/src/main/java/tamila/cl/ejemploandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package tamila.cl.ejemploandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
